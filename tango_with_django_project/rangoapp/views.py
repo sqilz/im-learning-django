5708802c01089e30deac9b0a729d01346c881500
Przemysław Hendel <przemyslaw.hendel@contractors.roche.com>
2018-07-04 09:33:08 +0200

    initial commit, djangotutorial project and tango_with_django_project

---
+++ tango_with_django_project/rangoapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("""Rango says het there partner
+     <a href='/rangoapp/about'>About</a>""")
+
+
+def about(request):
+    return HttpResponse("""Rango says here is the about page 
+                           <a href='/rangoapp/'> home</a>
+                        """)
