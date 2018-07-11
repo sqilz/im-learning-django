06bf0443bf9f3dbe8bd893cdef990028b0d50e77
Przemysław Hendel <przemyslaw.hendel@contractors.roche.com>
2018-07-11 08:01:35 +0200

    *models.py - typo fix

--- tango_with_django_project/rangoapp/models.py
+++ tango_with_django_project/rangoapp/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
-    class class Meta:
+    class Meta:
         # Fix typo 'Categorys' to Categories in admin page
         verbose_name_plural = 'Categories'
 
