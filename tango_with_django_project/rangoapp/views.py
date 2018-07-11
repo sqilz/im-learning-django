344b4decdb1d1d8752afdd7d3bbc9dd4472a0df0
Przemysław Hendel <przemyslaw.hendel@contractors.roche.com>
2018-07-11 13:38:52 +0200

    * views.py     modify the index view to list top 5 categories, and order them by the amount of likes using .objectsorder_by() * index.html     modified template to present stuff added in the     view     - listing categories with for loop OR error message

--- tango_with_django_project/rangoapp/views.py
+++ tango_with_django_project/rangoapp/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rangoapp.models import Category
 # Create your views here.
 
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
+    # Query the database for a list of ALL categories currently stored
+    # Order the categories by no. like in descending order
+    # Retrieve the top 5 only - or all if less than 5.
+    # place the list in our context_dict dictionary
+    # that will be passed to the template engine
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier
-    # Note that the first parameter is the template we wish to use.
+    # Render the response and send it back
     return render(request, 'rangoapp/index.html', context=context_dict)
 
 
