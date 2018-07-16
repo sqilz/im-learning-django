91ec5b7e01f51de238b4585fcf270f410c7cfe47
Przemysław Hendel <przemyslaw.hendel@contractors.roche.com>
2018-07-16 12:09:56 +0200

    *urls.py     app_name var -- acts as a    namespace *index.html     better template URL

--- tango_with_django_project/rangoapp/urls.py
+++ tango_with_django_project/rangoapp/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from rangoapp import views
 
+
+app_name = 'rangoapp'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
