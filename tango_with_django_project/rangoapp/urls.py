74919c721cd7c9d5421b91c5c248d6910e3e63fe
Przemysław Hendel <przemyslaw.hendel@contractors.roche.com>
2018-07-17 14:24:20 +0200

    adding a namespace to a single app project caused some problems, so it was commented out

--- tango_with_django_project/rangoapp/urls.py
+++ tango_with_django_project/rangoapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from rangoapp import views
 
 
-app_name = 'rangoapp'
+# app_name = 'rangoapp'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
